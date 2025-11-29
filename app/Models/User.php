f14f5faab87faa6b2dfac3106db268bc392b1007
masbirman <justme.freaks@gmail.com>
2025-11-29 22:05:27 +0800

    fix: Implement FilamentUser interface to allow admin access in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +47,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Allow access to admin panel for all users in this app
+        // You can add logic here like: return $this->email === 'admin@masbirman.web.id';
+        return true;
     }
 }
