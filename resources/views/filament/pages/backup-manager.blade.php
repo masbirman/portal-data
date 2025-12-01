a08c3d0732911c19bfe2bfb9c577fdae915b4e00
masbirman <justme.freaks@gmail.com>
2025-12-01 19:24:04 +0800

    fix: replace svg icon with emoji

--- resources/views/filament/pages/backup-manager.blade.php
+++ resources/views/filament/pages/backup-manager.blade.php
@@ -64,11 +64,7 @@
                 </div>
             @else
                 <div class="text-center py-8 text-gray-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-12 h-12 mx-auto mb-3 opacity-50">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z" />
-                    </svg>
+                    <p class="text-4xl mb-3">☁️</p>
                     <p>Belum ada backup tersimpan di Google Drive</p>
                 </div>
             @endif
